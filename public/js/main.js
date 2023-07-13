90bc5d70520e8f22b8fc411ca636fa2f7e1dd53d
BarkerEmily <emilybarker@byui.edu>
2023-07-13 15:31:47 -0600

    fixed "busy as a bee" and footer currentyear

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ $('#top-header').load('/views/partials/header.html')
 $('#footer').load('/views/partials/footer.html')
 
 // Footer Current Year Code
-//const today = new Date();
-//document.querySelector("#currentyear").textContent = today.getFullYear();
+// const today = new Date();
+// document.querySelector("#currentyear").textContent = today.getFullYear();
